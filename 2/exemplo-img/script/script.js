2b3ea84252d776ee8e3d6c4d35cc1e21f849e12c
Joice Flores Lazarino <flores.joice@gmail.com>
2018-09-19 16:40:27 -0300

    resolvido

---
+++ 2/exemplo-img/script/script.js
@@ -0,0 +1,12 @@
+const links = document.querySelector(".gallery__nav");
+console.log(links);
+console.log(links.children)
+
+for(let i = 0; i < links.children.length; i++){
+    console.log(links.children[i]);
+    links.children[i].addEventListener("click", function(){
+        const img = this.dataset.image;
+        console.log(img);
+        document.querySelector(".gallery__image").src = img;
+    })
+}
