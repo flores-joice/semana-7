ab57a609ec8d6502379f080d37a677cc754ee035
flores-joice <flores.joice@gmail.com>
2018-09-20 16:26:04 -0300

    resolução

--- 4/exemplo-1-twitter/script/script.js
+++ 4/exemplo-1-twitter/script/script.js
@@ -52,9 +52,19 @@
 const button = document.querySelector(".tweet-composer__button");
 const timeline = document.querySelector(".tweets-timeline");
 const texto = document.getElementById("tweetComposerInput");
+const counter = document.getElementById("tweetComposerCounter");
+
+texto.addEventListener("oninput", function(){
+    counter = counter - texto.length;
+    console.log(texto.length);
+})
 
 button.addEventListener("click", function(e){
     e.preventDefault();
+
+    if(texto.value === undefined || texto.value === null || texto.value === "" || texto.value === " "){
+        return false
+    }
 
    const newTweetBox = document.createElement("div");
 
@@ -66,10 +76,21 @@ button.addEventListener("click", function(e){
             <span class="tweets-timeline__date">1 de ago</span>
         </div>
         <p class="tweets-timeline__tweet">${texto.value}</p>
-        <div class="tweets-timeline__footer">tweet fixado</div>
+        <div class="tweets-timeline__footer">
+            <button class="tweets-timeline__delete-button">Excluir</button>
+        </div>
     </div>`;
 
    timeline.insertBefore(newTweetBox, timeline.childNodes[0]);
+
+   texto.value = "";
+
+   const botaoExcluir = document.querySelector(".tweets-timeline__delete-button");
+   botaoExcluir.addEventListener("click", function(excluir){
+       excluir.preventDefault();
+       newTweetBox.remove();
+
+   })
 
 })
 
